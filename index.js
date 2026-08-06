66c6e72e4e5c064210d0f5e7b4a8d7240a8499eb
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-08-06 21:27:27 +0600

    added product get and post api

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ dotenv.config();
 const app = express();
 const port = 3001 || process.env.PORT;
 
+app.use(express.json());
+
 const client = new MongoClient(`mongodb+srv://keebify:fBMbZ7iLe2npovek@cluster0.klq4o7m.mongodb.net/?appName=Cluster0`);
 
 export async function connectToMongoDB() {
     try {
         await client.connect();
         console.log("You successfully connected to MongoDB!");
+
+        const productsCollection = client.db('keebify').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const result = await productsCollection.find().toArray()
+            res.send(result)
+        })
+        
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            const result = await productsCollection.insertOne(product);
+            res.send(result)
+        })
+
         return client;
     } catch (err) {
         console.dir(err);
