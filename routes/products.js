e7c71932468831b801861f0085305c7bce16e93b
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-08-27 02:28:12 +0600

    added product edit route

--- routes/products.js
+++ routes/products.js
@@ -31,6 +31,29 @@ router.patch('/:id', async (req, res) => {
     res.send(result)
 })
 
+router.put('/:id', async (req, res) => {
+    const productsCollection = getDB().collection('products');
+    const {id} = req.params
+    const updatedProduct = req.body
+    const filter = {_id: new ObjectId(id)}
+    const newProduct = {
+        $set: {
+            name: updatedProduct.name,
+            price: updatedProduct.price,
+            category: updatedProduct.category,
+            colors: updatedProduct.colors,
+            stock: updatedProduct.stock,
+            status: updatedProduct.status,
+            review: updatedProduct.review,
+            description: updatedProduct.description,
+            photos: updatedProduct.photos
+        }
+    }
+    const options = { upsert: true }
+    const result = await productsCollection.updateOne(filter, newProduct, options)
+    res.send(result)
+})
+
 router.delete('/:id', async (req, res) => {
     const productsCollection = getDB().collection('products');
     const {id} = req.params
